fbec5780d6c25f368e9ac430b2b4b58fda4cd3d0
DaniloLMD <danilolmd04@gmail.com>
2025-04-04 08:48:59 -0300

    updating template

--- template/template.cpp
+++ template/template.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define ld long double
+using ll = long long;
+using ld = long double;
 #define dbg(x) cout << #x << " = " << x << endl;
 #define all(v) v.begin(), v.end()
 
 mt19937 rng((int) chrono::steady_clock::now().time_since_epoch().count());
 
-void solve(){
+void test(){
 
 }
 
@@ -19,7 +19,7 @@ int32_t main(){
     int t = 1;
     // cin >> t;
     while(t--){
-        solve();
+        test();
     }
 
     return 0;
